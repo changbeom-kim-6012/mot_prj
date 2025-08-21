157f7fd98f8b8d42415915e59bb39cdc3a101f39
신상호 <shshin@erns.co.kr>
2025-08-21 15:40:12 +0900

    Update mot-hub-pm2.config.js

--- mot-hub-pm2.config.js
+++ mot-hub-pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "MOT_DEV",                  // PM2에서 식별할 이름
+      name: "MOT_HUB",                  // PM2에서 식별할 이름
       script: "npm",
       args: "run startDev",              // 실행할 npm 스크립트
       cwd: "/var/lib/jenkins/workspace/mot-front",  // 작업 디렉토리
